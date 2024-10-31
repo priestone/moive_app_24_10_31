0cf8184c1763781ba3934bc73af1633d9d72da44
priestone <aion97@naver.com>
2024-10-31 15:29:13 +0900

    api useState 저장 완료

--- src/api.js
+++ src/api.js
@@ -1,19 +1,28 @@
-const fetch =require("node-fetch");
+const fetch = require("node-fetch");
 
 const baseUrl = "https://api.themoviedb.org/3/";
 
 const options = {
-    method: "GET",
-    headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYTQyZGRkODVkZjBjNWM4MGZiYzU5NmUwMGVlOTZiMyIsIm5iZiI6MTczMDI2NjE3Ny4zNzM1OTU3LCJzdWIiOiI2NzIxYjQ2ODk3NGE2NzZjNmRmMzNiNzIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.nAclr972JkfTrNpc1xw0KnqMkFZp8vx1bVfBNZr0Jkk'
-    },
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYTQyZGRkODVkZjBjNWM4MGZiYzU5NmUwMGVlOTZiMyIsIm5iZiI6MTczMDI2NjE3Ny4zNzM1OTU3LCJzdWIiOiI2NzIxYjQ2ODk3NGE2NzZjNmRmMzNiNzIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.nAclr972JkfTrNpc1xw0KnqMkFZp8vx1bVfBNZr0Jkk",
+  },
 };
 
 const url = (urlName) => {
-    return baseUrl + `${urlName}?language=ko-kr`
-}
+  return baseUrl + `${urlName}?language=ko-kr`;
+};
 
-export const nowPlaying = () => fetch(url("movie/now_playing"),options).then((res)=>res.json());
+export const nowPlaying = () =>
+  fetch(url("movie/now_playing"), options).then((res) => res.json());
 
-export const popular = () => fetch(url("movie/popular"),options).then(res => res.json());
+export const popular = () =>
+  fetch(url("movie/popular"), options).then((res) => res.json());
+
+export const topRated = () =>
+  fetch(url("movie/top_rated"), options).then((res) => res.json());
+
+export const upComing = () =>
+  fetch(url("movie/upcoming"), options).then((res) => res.json());
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,31 +1,36 @@
-import { useEffect, useState} from "react";
-import { nowPlaying, popular } from "../../api";
+import { useEffect, useState } from "react";
+import { nowPlaying, popular, topRated, upcoming } from "../../api";
 
 const Home = () => {
-    const [nowData, setNowData] = useState();
-    const [popData, setPopData] = useState();
+  const [nowData, setNowData] = useState();
+  const [popData, setPopData] = useState();
+  const [topData, setTopData] = useState();
+  const [upData, setUpData] = useState();
 
+  useEffect(() => {
+    (async () => {
+      try {
+        const { results: now } = await nowPlaying();
+        const { results: pop } = await popular();
+        const { results: top } = await topRated();
+        const { results: up } = await upComing();
 
-    useEffect( () => {
-    (async ()=>{
-        try {
-            const {results:now} = await nowPlaying();
-            const {results:pop} = await popular();
-    
-            setNowData(now);
-            setPopData(pop);
-        } catch (error) {
-            console.log(error);
-        }
-    })()
-    }, [])
+        setNowData(now);
+        setPopData(pop);
+        setTopData(top);
+        setUpData(up);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
 
-    console.log(nowData);
-    console.log(popData);
+  console.log(nowData);
+  console.log(popData);
+  console.log(topData);
+  console.log(upData);
 
-    return(
-    <div>Home</div>
-)
-}
+  return <div>Home</div>;
+};
 
 export default Home;
