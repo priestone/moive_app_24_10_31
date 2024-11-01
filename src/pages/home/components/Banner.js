e6c120c7be727fe6189a2789831a5f16b15a9cbf
사재원 <aion97@naver.com>
2024-11-01 22:17:14 +0900

    random 적용

--- src/pages/home/components/Banner.js
+++ src/pages/home/components/Banner.js
@@ -44,11 +44,13 @@ const TitleWrap = styled.div`
   }
 `;
 const Banner = ({ data }) => {
+  let random = Math.floor(Math.random() * 21);
+
   return (
-    <MainBanner $coverImg={data[0].backdrop_path}>
+    <MainBanner $coverImg={data[random].backdrop_path}>
       <TitleWrap>
-        <h3>{data[0]?.title}</h3>
-        <p>{data[0]?.overview.slice(0, 100) + "..."}</p>
+        <h3>{data[random]?.title}</h3>
+        <p>{data[random]?.overview.slice(0, 100) + "..."}</p>
       </TitleWrap>
     </MainBanner>
   );
